7da3022fa1fa9fed90f1b9f9b764a6b6efce739c
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2017-03-08 17:11:22 -0600

    syscall.py: plot top-N syscall per task
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>

--- syscall.py
+++ syscall.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
-syscall_dict = {}
-
 import glob
+import operator
+
+tasks = dict()
 
 strace_files = glob.glob('*.do_*')
 
 for f in strace_files:
+    task = f.split('.')[-2]
+    if not tasks.has_key(task):
+        tasks[task] = dict()
+    syscall_dict = tasks[task]
     with open(f) as fd:
         fd.readline()
         fd.readline()
@@ -30,5 +35,11 @@ for f in strace_files:
                 syscall_dict[syscall] += seconds
 
 
-for syscall, seconds in syscall_dict.items():
-    print '%s %s' % (seconds, syscall)
+sorted_tasks = dict()
+for task, data in tasks.items():
+    sorted_tasks[task] = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
+
+for task, data in sorted_tasks.items():
+    for i in xrange(5):
+        syscall, seconds = data[i]
+        print '%s %s %s' % (task, syscall, seconds)
