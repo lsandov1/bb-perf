23674ef59ee18498161563f5065561bb4fd40962
Leonardo Sandoval <leonardo.sandoval.gonzalez@linux.intel.com>
2017-03-09 09:10:59 -0600

    Revert "strace.py: include command line parser"
    This reverts commit 67ec6ab3437eeed735b4461579c898cf75d348a0.

--- strace.sh
+++ strace.sh
@@ -1,54 +1,65 @@
-#!/usr/bin/python
+#!/bin/sh
 
-import sys
-import glob
-import operator
-import argparse
+# plot HW info
+lscpu
+free
 
-def main(top):
-    tasks = dict()
-    strace_files = glob.glob('*.do_*')
+# make sure DL_DIR parameter is provided
+if [ ! $# -eq 1 ]; then
+    echo "$0 DL_DIR"
+    exit 1
+fi
 
-    for f in strace_files:
-        task = f.split('.')[-2]
-        if not tasks.has_key(task):
-            tasks[task] = dict()
-        syscall_dict = tasks[task]
-        with open(f) as fd:
-            fd.readline()
-            fd.readline()
-            for line in fd:
+# check tools
+which vmstat > /dev/null || { echo "Install vmstat before running $0"; exit 1; }
 
-                if '---' in line:
-                    break
+# download directory
+DL_DIR=$1
+DL_DIR=$(realpath -e $DL_DIR)
 
-                seconds = None
-                syscall = None
-                data = line.split()
-                if len(data) == 6:
-                    time, seconds, usec_per_call, calls, errors, syscall = data
-                else:
-                    time, seconds, usec_per_call, calls, syscall = data
+# clone poky
+if [ ! -d poky ]; then
+    git clone git://git.yoctoproject.org/poky
+fi
 
-                seconds = float(seconds)
-                if not syscall_dict.has_key(syscall):
-                    syscall_dict[syscall] = seconds
-                else:
-                    syscall_dict[syscall] += seconds
+# patch
+cd poky
+git am ../patches/0001-buildhistory.bbclass-strace-c-every-task.patch
+if [ $? -ne 0 ]; then
+   echo "strace not enable, patch $STRACE_PATCH manually or check if already present"
+   exit 1
+fi
 
+# fetch those missing upstream projects
+[ -d $PWD/build ] && { echo "ERROR: Build folder ($PWD/build) present, remove it and execute $0 again";  exit 1; }
+. $PWD/oe-init-build-env build
+cat > conf/auto.conf << EOF
+DL_DIR = "$DL_DIR"
+EOF
+bitbake core-image-minimal -c fetchall
 
-    #sort data
-    sorted_tasks = dict()
-    for task, data in tasks.items():
-        sorted_tasks[task] = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
+# disable network access
+cat >> conf/auto.conf << EOF
+BB_NO_NETWORK = "1"
+EOF
 
-    for task, data in sorted_tasks.items():
-        for i in xrange(1):
-            syscall, seconds = data[i]
-            print '%s %s %s' % (task, syscall, seconds)
+# start build in background
+bitbake core-image-minimal 1>/dev/null
+pid_bitbake=$!
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Sum syscall per bb tasks and print the top ones')
-    parser.add_argument('--top', dest='top', default=1, help='top n')
-    args = parser.parse_args()
-    sys.exit(main(args.top))
+# If this script is killed, kill both process.
+trap "kill $pid_bitbake 2> /dev/null" EXIT
+trap "kill $pid_bitbake 2> /dev/null" INT
+
+cat <<EOF
+
+strace data is collected into /tmp/strace.
+
+# Forky tasks
+grep -r execve /tmp/strace | awk '{print $5,$1}' | sort -n -r | head -25
+
+EOF
+
+# Disable the trap on a normal exit.
+trap - EXIT
+trap - INT
